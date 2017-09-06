e63c6a69698fbd43eba66122c2ecd7f479c9107d
Shawn Carew <tkamen@nl.rogers.com>
2017-09-06 19:23:27 -0230

    Wedges begun

---
+++ Assets/Scripts/Boss/Boss_Movement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Boss_Movement : MonoBehaviour {
+
+	private GameManager gm;
+	private Rigidbody rb;
+	private enum BossType {Wedge, Spider, Mothership, Other};
+	[SerializeField] private BossType myType = BossType.Wedge;
+	[SerializeField] private float moveSpeed = 500f;
+	private float changeDirection = 0f;
+	private Vector3 targetVector;
+	private bool bSpeedUp = false;
+	private float slowdownTime = 2.0f;
+
+	void Start () {
+		gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+		rb = GetComponent<Rigidbody>();
+
+		if (myType == BossType.Wedge) {
+			//moveSpeed = 1f;
+			targetVector = SetTarget();
+
+		}
+
+		//rb.AddForce((targetVector - transform.position) * moveSpeed, ForceMode.Force);
+
+	}
+
+	Vector3 SetTarget() {
+		Vector3 wayPoint = Vector3.zero;
+
+		if (myType == BossType.Wedge) {
+			float h = gm.level_height;
+			float w = gm.level_width;
+			wayPoint = new Vector3(Random.Range(-w/2, w/2), Random.Range(-h/2, h/2), 0f);
+		}
+		//rb.velocity = Vector3.zero;
+		changeDirection = slowdownTime;
+		bSpeedUp = false;
+		return wayPoint;
+
+	}
+
+	void Update () {
+		if (transform.position.z != 0f) {
+			transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, 0f), 0.1f);
+		}
+		if (((int)(transform.position.x*10) == (int)(targetVector.x*10)) || ((int)(transform.position.y*10) == (int)(targetVector.y*10))) {
+			targetVector = SetTarget();
+			//rb.AddForce((targetVector - transform.position) * moveSpeed, ForceMode.Force);
+			//Debug.Log("Vel:" + rb.velocity);
+		}
+		if (changeDirection > 0f) {
+			changeDirection -= Time.deltaTime;
+			if (changeDirection < 0f) { changeDirection = 0f; }
+			if (!bSpeedUp) {
+				rb.velocity = rb.velocity * (changeDirection/slowdownTime); //decrease velocity;
+			} else {
+				rb.velocity = Vector3.zero;
+				rb.AddForce(targetVector * moveSpeed, ForceMode.Force);
+				rb.velocity = rb.velocity * ((slowdownTime - changeDirection)/slowdownTime); //increase velocity
+			}
+			if (changeDirection == 0f && !bSpeedUp) {
+				bSpeedUp = true;
+				changeDirection = slowdownTime;
+			}
+		}
+		//transform.position = Vector3.MoveTowards(transform.position, targetVector, moveSpeed * Time.deltaTime);
+
+		//Debug.Log("Pos:" + transform.position + "  Tar:" + targetVector);
+	}
+}
---
+++ Assets/Scripts/Boss/Boss_Wedge.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Boss_Wedge : MonoBehaviour {
+
+	private Transform[] wedges;
+	private float[] x0, y0, x1, y1;
+
+	private float driftSpeed = 0.003f;
+	private float driftRange = 0.5f;
+
+	void Start () {
+		wedges = new Transform[101];
+		MeshRenderer[] wedgesMesh = gameObject.GetComponentsInChildren<MeshRenderer>();
+		int j=0;
+		for (int i=0; i < wedgesMesh.Length; i++) {
+			if (wedgesMesh[i].tag == "Untagged") { 
+				wedges[j] = wedgesMesh[i].gameObject.transform; 
+				Debug.Log(i + " == " + wedges[j].name + " (" + wedgesMesh[i].name + ")");
+				j++;
+			}
+		}
+		wedgesMesh = null;
+		x0 = new float[wedges.Length];
+		y0 = new float[wedges.Length];
+		x1 = new float[wedges.Length];
+		y1 = new float[wedges.Length];
+		for (int i=0; i < wedges.Length; i++) {
+			x0[i] = wedges[i].localPosition.x;
+			y0[i] = wedges[i].localPosition.y;
+
+			x1[i] = 1.1f * x0[i] + Random.Range(-driftRange, driftRange);
+			y1[i] = 1.1f * y0[i] + Random.Range(-driftRange, driftRange);
+		}
+
+	}
+	
+	void Update () {
+		for (int i=0; i < wedges.Length; i++) {
+			wedges[i].localPosition = Vector3.MoveTowards(wedges[i].localPosition, new Vector3(x1[i], y1[i], wedges[i].localPosition.z), driftSpeed);
+			if (wedges[i].localPosition.x == x1[i] && wedges[i].localPosition.y == y1[i]) {
+				float tmp1 = x1[i];
+				float tmp2 = y1[i];
+				x1[i] = x0[i];
+				y1[i] = y0[i];
+				x0[i] = tmp1;
+				y0[i] = tmp2;
+			}
+		}
+	}
+}
